406203d7dc1afbc8e3bbe26f63a79339e6a28112
RealLanta <me@lanta.cyou>
2022-06-18 17:38:51 +0800

    Remove edit button

--- src/pages/Sponsor.tsx
+++ src/pages/Sponsor.tsx
@@ -2,7 +2,7 @@ import S3 from 'aws-sdk/clients/s3';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { useCallback, useRef, useState } from 'react';
-import Edit from '../components/edit';
+// import Edit from '../components/edit';
 import { ErrorDialog } from '../components/error';
 import { ShareLinkDialog } from '../components/home/ShareLinkDialog';
 import Malleable, { FieldEdit } from '../components/malleable';
@@ -125,7 +125,7 @@ export default function Home(props) {
   return (
     <>
       <Head>
-        <title>ArchLinux.cyou | Sponsor</title>
+        <title>ArchLinux.cyou</title>
         <meta
           name="description"
           content="Free subdomain for Arch Linux lovers!"
@@ -172,17 +172,6 @@ export default function Home(props) {
           <Content isEdit={isEdit} edits={edits} />
         )}
       </div>
-      {isEdit ? (
-        <>
-          <Snapshot
-            onCancel={toggleEdit}
-            onShare={share}
-            isSharing={isSharingView}
-          />
-        </>
-      ) : (
-        <Edit onClick={toggleEdit} />
-      )}
     </>
   );
 }
